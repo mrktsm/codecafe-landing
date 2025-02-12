f13de9053ea40d2aa4e118f3d053ec754cb8a138
Marko Tsymbaluk <markotsymbaluk@gmail.com>
2025-02-12 00:49:03 -0500

    Add Stone Backround And White Rays

---
+++ src/app/components/ui/aurora-background.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { cn } from "../../lib/utils";
+import React, { ReactNode } from "react";
+
+interface AuroraBackgroundProps extends React.HTMLProps<HTMLDivElement> {
+  children: ReactNode;
+  showRadialGradient?: boolean;
+}
+
+export const AuroraBackground = ({
+  className,
+  children,
+  showRadialGradient = true,
+  ...props
+}: AuroraBackgroundProps) => {
+  return (
+    <main>
+      <div
+        className={cn(
+          "relative flex flex-col h-[100vh] items-center justify-center bg-stone-900 text-slate-50 transition-bg",
+          className
+        )}
+        {...props}
+      >
+        <div className="absolute inset-0 overflow-hidden">
+          <div
+            className={cn(
+              `
+            [--dark-base:repeating-linear-gradient(100deg,var(--stone-900)_0%,var(--stone-900)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--stone-900)_16%)]
+            [--aurora:repeating-linear-gradient(100deg,var(--blue-900)_10%,var(--white)_15%,var(--blue-800)_20%,var(--white)_25%,var(--blue-900)_30%)]
+            [background-image:var(--dark-base),var(--aurora)]
+            [background-size:300%,_200%]
+            [background-position:50%_50%,50%_50%]
+            filter blur-[10px]
+            after:content-[""] after:absolute after:inset-0 after:[background-image:var(--dark-base),var(--aurora)]
+            after:[background-size:200%,_100%] 
+            after:animate-aurora after:[background-attachment:fixed] after:mix-blend-difference
+            pointer-events-none
+            absolute -inset-[10px] opacity-50 will-change-transform`,
+
+              showRadialGradient &&
+                `[mask-image:radial-gradient(ellipse_at_100%_0%,black_10%,var(--transparent)_70%)]`
+            )}
+          ></div>
+        </div>
+        {children}
+      </div>
+    </main>
+  );
+};
